f2553dea0b59548ef83968b67649a3b1cb7b6256
ANKIT12019 <rajankit12019@gmail.com>
2025-04-11 03:16:19 +0530

    commit2

--- app/(admin)/admin/page.jsx
+++ app/(admin)/admin/page.jsx
@@ -2,8 +2,8 @@ import { getDashboardData } from "@/actions/admin";
 import { Dashboard } from "./_components/dashboard";
 
 export const metadata = {
-  title: "Dashboard | Vehiql Admin",
-  description: "Admin dashboard for Vehiql car marketplace",
+  title: "Dashboard | GearGo Admin",
+  description: "Admin dashboard for GearGo car marketplace",
 };
 
 export default async function AdminDashboardPage() {
--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Vehiqle",
+  title: "GearGoe",
   description: "Find your Dream Car",
 };
 
